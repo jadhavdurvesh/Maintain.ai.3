c05dc9d2915ad02390d6de264182b84b8d15d8b3
Durvesh <89296288+jadhavdurvesh@users.noreply.github.com>
2026-09-10 21:30:29 +0530

    Load shared skeleton styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import { HashRouter } from 'react-router-dom'
 import { AuthProvider } from './AuthContext.jsx'
 import App from './App.jsx'
 import './index.css'
+import './skeleton.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
